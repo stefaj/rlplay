2d289c7c61b717a4561ab8ee48abee8f1486d4f4
Stefan Jacholke <stefanjacholke@gmail.com>
2017-11-07 16:10:36 +0200

    Fix critic_grad

--- dpg.py
+++ dpg.py
@@ -7,43 +7,43 @@ num_actions = 2
 num_obs = 4
 gamma = 0.9
 
+policy_qhat = tf.placeholder('float',[None, 1],name='policy_qhat')
 state_inp = tf.placeholder('float', [None, num_obs],name='state_inp')
 policy_l1 = tf.layers.dense(inputs=state_inp, units=50, activation=tf.nn.relu)
 policy_out = tf.layers.dense(inputs=policy_l1, units=num_actions, activation=tf.nn.softmax)
-policy_qhat = tf.placeholder('float',[None, 1],name='policy_qhat')
 
 act_inp = tf.placeholder('float',shape=[None, num_actions])
-# sel_act = tf.argmax(act_inp, output_type=tf.int32, axis=1)
-# indices = tf.range(0, tf.shape(policy_out)[0]) * tf.shape(policy_out)[1] + sel_act
-# sel_act_probs = tf.gather(tf.reshape(policy_out, [-1]), indices)
-sel_act_probs = tf.add(tf.multiply(act_inp, policy_out),1e-6)
+sel_act_probs = tf.log( tf.add(tf.multiply(act_inp, policy_out),1e-6) )
 
+policy_vars = tf.trainable_variables()
+policy_var_len = len(policy_vars)
 
+cstate_inp = tf.placeholder('float', [None, num_obs],name='cstate_inp')
 critic_actions = tf.placeholder('float', [None, num_actions])
 critic_targ = tf.placeholder('float', [None, 1])
-critic_prestate = tf.layers.dense(inputs=state_inp, units=21, activation=tf.nn.relu)
-critic_params = tf.get_variable('critic_params', [21, 5])
-critic_paramact = tf.matmul(tf.layers.dense(inputs=critic_actions, units=21, activation=tf.nn.relu)
-        , critic_params)
-linear = tf.matmul(critic_prestate, critic_params)
-critic_l1 = tf.layers.dense(inputs=tf.concat([linear, critic_prestate],axis=1), units=10, activation=tf.nn.relu)
-
-# critic_l2 = tf.layers.dense(inputs=state_inp, units=50, activation=tf.nn.relu)
-# critic_l3 = tf.layers.dense(inputs=tf.concat([critic_l2, critic_actions],axis=1), units=20, activation=tf.nn.relu)
-# critic_l4 = tf.layers.dense(inputs=critic_l3, units=5, activation=tf.nn.relu)
-
+critic_paramact = tf.layers.dense(inputs=critic_actions, units=num_obs, activation=tf.nn.relu)
+linear = tf.layers.dense(inputs=critic_paramact*cstate_inp, units=100, activation=tf.nn.relu)
+critic_l1 = tf.layers.dense(inputs=linear, units=50, activation=tf.nn.relu)
 critic_out = tf.layers.dense(inputs=critic_l1, units=1, activation=None)
+
+
+cur_vars = tf.trainable_variables()
+critic_vars = cur_vars[policy_var_len:]
+critic_var_len = len(critic_vars)
+
+critic_grads = tf.gradients(critic_l1, critic_actions)
+# print(critic_grads)
+# raise 'poes'
 critic_loss = tf.nn.l2_loss(critic_targ - critic_out)
-critic_opt = tf.train.AdamOptimizer(0.001).minimize(critic_loss)
+critic_opt = tf.train.AdamOptimizer(0.0010).minimize(critic_loss)
 
 policy_crit_grad = tf.placeholder('float', [None, num_actions]) 
-policy_loss = -tf.reduce_mean( tf.multiply(policy_qhat, tf.log(sel_act_probs) ) )
-policy_grads = tf.gradients(policy_loss, tf.trainable_variables())
-# critic_grads = tf.gradients(critic_loss, tf.trainable_variables())
-# deter_grads = tf.multiply(policy_grads, critic_grads)
+policy_loss = -tf.reduce_mean( tf.multiply(policy_qhat, sel_act_probs ) )
+policy_grads = tf.gradients(policy_loss, policy_vars)
+grads = tf.gradients(policy_out,policy_vars )
+
 policy_opt_ = tf.train.AdamOptimizer(0.01)
-# policy_opt = policy_opt_.minimize(policy_loss)
-apply_grads = policy_opt_.apply_gradients(grads_and_vars=zip(policy_grads,tf.trainable_variables()))
+apply_grads = policy_opt_.apply_gradients(grads_and_vars=zip(policy_grads,policy_vars))
 
 
 sess = tf.Session()
@@ -76,7 +76,7 @@ def discount_rewards(rewards):
     return rs
 
 def get_advantage(actions, states,rewards):
-    qs = sess.run(critic_out, feed_dict={state_inp:states, critic_actions:actions })
+    qs = sess.run(critic_out, feed_dict={cstate_inp:states, critic_actions:actions })
     rs = []
     for (q,r) in zip(qs,rewards): 
         rs.append([r-q[0]])
@@ -101,9 +101,12 @@ for i in range(0,10000):
     # print('states',states)
     # print('actions',actions)
     # print('target',rewards)
-    sess.run(apply_grads, feed_dict={state_inp: states, act_inp: actions, policy_qhat:rewards} )
     targ = [ [r] for r in discount_rewards(rs) ]
-    sess.run(critic_opt, feed_dict={state_inp: states, critic_targ:targ, critic_actions:actions} )
+    sess.run(critic_opt, feed_dict={cstate_inp: states, critic_targ:targ, critic_actions:actions} )
+    cgrads = sess.run(critic_grads, feed_dict={cstate_inp:states, critic_actions:actions })
+
+    sess.run(apply_grads, feed_dict={state_inp: states, act_inp: actions, policy_qhat:rewards} )
+    pgrads = sess.run(grads, feed_dict={state_inp:states})
 
     if i % 10 == 0: print("Iteration %d Rolloing Reward: %d" % (i,rr))
 
